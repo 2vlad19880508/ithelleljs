35563f9ff1853243e4685b4d61fb43fa08615a1f
vivanov <vlad19880508@gmail.com>
2024-10-09 06:09:04 +0300

    test

---
+++ lesson3.js
@@ -0,0 +1,33 @@
+let a = null;
+const b = "Hello!";
+let password = "123";
+let str2 = `Your password: ${password}`;
+let str3 = "Your password: " + password;
+// var c = "Test"; Old school
+console.log(str2);
+console.log(str3);
+
+
+
+a = 2;
+console.log(a);
+
+let cat = "cat";
+let dog = "dog";
+
+let bool = true;
+let bool2 = false;
+
+let animalsArr = [cat, "dog", 2, bool, password, ["test", 555]];
+
+console.log(cat);
+console.log(animals[0]);
+console.log(animals[1]);
+console.log(animals[2]);
+console.log(animals[3]);
+console.log(animals[4]);
+console.log(animals[5][0]);
+console.log(animals);
+
+animals = 2
+console.log(animals);
---
+++ lesson4.js
@@ -0,0 +1,124 @@
+// console.log(0.1 + 0.2 === 0.3);
+// console.log((0.1 + 0.2).toFixed(4));
+
+// const randomNum = Math.random(); // Поверне випадкове число між 0 та 1
+// console.log(randomNum);
+
+// console.log(0.1 + 0.2 == 0.3);// false
+
+
+
+// const text = "1234567";
+// const arr = [2, []];
+// console.log(text);
+// console.log(arr);
+// console.log(text[0]);
+
+// console.log(arr[0]);
+
+// console.log(text + 'test');
+// let newArr = false + true;
+// console.log(newArr);
+// console.log(newArr[5])
+// console.log(arr[5])
+
+// const arr = ['H', 'e', 'l', 'l', 'o']
+// const str = "Hello";
+// const lastIndex = str.length - 1
+
+// // останній символ
+// console.log( str[lastIndex] ); // o
+
+// // Або передавши вираз вирахування індексу одразу в квадратні дужки
+
+// // останній символ
+// console.log( str[str.length - 1] ); // o
+
+// // передостанній символ
+// console.log( str[str.length - 2] ); // l
+
+// // останній символ
+// console.log( arr[lastIndex] ); // o
+
+// // Або передавши вираз вирахування індексу одразу в квадратні дужки
+
+// // останній символ
+// console.log( arr[str.length - 1] ); // o
+
+// // передостанній символ
+// console.log( arr[str.length - 2] ); // l
+
+
+
+// const testData = 123;
+
+// let a = 'Test number = ' + testData
+// console.log(a)
+
+
+
+// let b, c, d;
+
+// c = `Test num = ${testData}`;
+
+// b = a + testData;
+
+// d = a + testData + b + 99999 + true;
+
+
+// console.log(b);
+// console.log(c);
+// console.log(d)
+
+
+
+// let counter = 0;
+// counter++; // Інкремент-працює так само, як counter = counter + 1, але запис коротше
+// console.log(counter); // 3
+
+// console.log(3 === 3); 
+// console.log(3 == 3); 
+// console.log(3 === "3"); 
+// console.log(3 == "3");
+
+// console.log(1 === true);
+
+// let a = 2;
+// let b = 3;
+
+// console.log(a >= 2 && b === 3);
+
+
+
+// let a = 11; //присвоюємо змінній a значення 11
+// console.log(a);
+
+// let b, c, d;
+// console.log(b,c,d); // 11, 10, 10, 10
+
+
+// b = c = d = a = 10; // "ланцюжок" операторів присвоєння:
+
+// console.log(a,b,c,d); // 11, 10, 10, 10
+
+// let a = 42;
+// //a += 2;
+
+// console.log('Hello' === 'World' ? 1 : 0) // 0
+
+
+
+// a *= 2;
+// a /= 2;
+
+// console.log(a)
+
+
+// console.log(true || true); // true
+// console.log(false || true); // true
+// console.log(true | false); // true
+// console.log(false | false); // false
+
+let a = 1000000;
+
+console.log(a);
